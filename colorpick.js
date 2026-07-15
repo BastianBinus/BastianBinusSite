e075876e65b08aa7e0f828b9f0775ab7b20d1da2
Bastian <bastian.binus@pm.me>
2026-07-15 11:52:12 +0200

    feat: add hex/rgb output and fix element id typo

--- colorpick.js
+++ colorpick.js
@@ -2,15 +2,24 @@ function updatePreview() {
     const hue = document.getElementById("Hue").value;
     const sat = document.getElementById("Saturation").value;
     const light = document.getElementById("Lightness").value;
-    
     document.querySelector(".color-preview span").style.backgroundColor = `hsl(${hue}, ${sat}%, ${light}%)`;
+    let rgbVal = hslToRgb(hue/360, sat/100, light/100)
+    let rVal = rgbVal[0]
+    let gVal = rgbVal[1]
+    let bVal = rgbVal[2]
+    let HexOut = rgbToHex(rVal, gVal, bVal)
+    let rgbOut = rgbOutputFunction(rVal, gVal, bVal)
+    document.getElementById("hexOutput").value = HexOut;
+    document.getElementById("rgbOutput").value = rgbOut;
 }
 
 document.getElementById("Hue").addEventListener("input", function() {
-    const hueValue=this.value
+    const hueValue = this.value;
     document.getElementById("Saturation").style.setProperty("--hue", hueValue);
     document.getElementById("Lightness").style.setProperty("--hue", hueValue);
-    document.getElementById("Hue").addEventListener("input", updatePreview);
+});
+
+document.getElementById("Hue").addEventListener("input", updatePreview);
 document.getElementById("Saturation").addEventListener("input", updatePreview);
 document.getElementById("Lightness").addEventListener("input", updatePreview);
-});
+
