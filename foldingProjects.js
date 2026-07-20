0bf3cfac84d9c89d58fabf16fbac47da8381e8c3
Bastian <bastian.binus@pm.me>
2026-07-20 15:02:20 +0200

    Feat/ChangingSrcFromImg

--- foldingProjects.js
+++ foldingProjects.js
@@ -3,9 +3,17 @@ const allButtons = document.querySelectorAll('#Projects-Container .btn')
 allButtons.forEach(PrBtn => {
   PrBtn.addEventListener('click', function() {
     let conAc = this.classList.contains('active')
-    allButtons.forEach(btn => btn.classList.remove('active'))
+    
+  allButtons.forEach(btn => {
+  if (!btn.querySelector('img')) return;
+  const img = btn.querySelector('img');
+  img.src = img.src.replace('Big', 'Small');
+  btn.classList.remove('active');
+})
     if (!conAc){
       this.classList.add('active');
+      let img = this.querySelector('img');
+      img.src = img.src.replace('Small', 'Big');
     } else {
       this.classList.remove('active');
     }
