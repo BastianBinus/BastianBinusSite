7ad3f6d976481919e436361f915c11c47ec81005
Bastian <bastian.binus@pm.me>
2026-07-28 14:50:40 +0200

    fix: update stylelint and related configurations
    refactor: update stylesheet link in play.html
    
    style: modify CSS selectors for project previews and add new styles
    
    feat: enhance work.html with project descriptions and previews
    
    chore: add new stylesheet for Public Site Generator
    
    chore: create index.html for Public Site Generator with layout and content
    
    chore: create process.html for Public Site Generator detailing the build process

--- foldingProjects.js
+++ foldingProjects.js
@@ -1,22 +1,22 @@
-const allButtons = document.querySelectorAll('#Projects-Container .btn')
+const allButtons = document.querySelectorAll("#Projects-Container .btn");
 
-allButtons.forEach(PrBtn => {
-  PrBtn.addEventListener('click', function() {
-    let conAc = this.classList.contains('active')
-    
-  allButtons.forEach(btn => {
-    btn.classList.remove('active');
-    if (!btn.querySelector('img')) return;
-    const img = btn.querySelector('img');
-    img.src = img.src.replace('Big', 'Small');
-})
-    if (!conAc){
-      this.classList.add('active');
-      if (!this.querySelector('img')) return;
-      let img = this.querySelector('img');
-      img.src = img.src.replace('Small', 'Big');
+allButtons.forEach((PrBtn) => {
+  PrBtn.addEventListener("click", function () {
+    let conAc = this.classList.contains("active");
+
+    allButtons.forEach((btn) => {
+      btn.classList.remove("active");
+      if (!btn.querySelector("img")) return;
+      const img = btn.querySelector("img");
+      img.src = img.src.replace("Big", "Small");
+    });
+    if (!conAc) {
+      this.classList.add("active");
+      if (!this.querySelector("img")) return;
+      let img = this.querySelector("img");
+      img.src = img.src.replace("Small", "Big");
     } else {
-      this.classList.remove('active');
+      this.classList.remove("active");
     }
-  })
+  });
 });
